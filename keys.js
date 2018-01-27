58be398f71f5614b0503040f273bfc4347246896
rdrachenberg <31778670+rdrachenberg@users.noreply.github.com>
2018-01-26 20:12:13 -0500

    backup, partually working

--- keys.js
+++ keys.js
@@ -1,4 +1,5 @@
 console.log('this is loaded');
+console.log('this is actually working')
 
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
--- liri.js
+++ liri.js
@@ -1,2 +1,26 @@
 // require dotenv package 
 require("dotenv").config();
+
+// require keys.js file
+var keys = require('./keys.js');
+
+// creating a variable that requries 'dotenv' 
+var dotenv = require('dotenv')
+
+//dont know if i need the below variable with require 
+var enviro = require('./enviro.env')
+
+// creating the new object 
+var buf = new Buffer('BASIC=basic')
+
+// returns the object
+var config = dotenv.parse(buf) 
+
+//print the object to the console
+console.log(typeof config, config)
+
+// create variable to access key information 
+var spotify = new Spotify(keys.spotify)
+var client = new Twitter(keys.twitter)
+
+console.log(spotify)
