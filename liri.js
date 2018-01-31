a6a05568a6526dece3642966cd6994ba9b636c0c
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-01-31 00:19:05 -0500

    omdb and twitter calls working

--- liri.js
+++ liri.js
@@ -4,6 +4,9 @@ var request = require("request");
 // require dotenv package 
 require('dotenv').config();
 
+// create variable params for twitter
+var params = { rdrac23: 'nodejs' };
+
 // require keys.js file
 var keys = require('./keys.js');
 
@@ -19,33 +22,55 @@ var Spotify = require('node-spotify-api');
 // create variable twitter that requires twitter npm package 
 var Twitter = require('twitter');
 
-// // creating the new object 
-// var buf = new Buffer('BASIC=basic');
-
-// returns the object
-// var config = dotenv.parse(buf); 
-
-//print the object to the console
-// console.log(typeof config, config);
-
 // create variable to access key information 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-// create reqeust from omdb
+// create variable with argv, variable for query reqeust from omdb
+// then the actual request 
+var nodeArgs = process.argv;
 
-var movie = process.argv[2];
+var movie = "";
 
-var apikey = '7fc0d809'
+    for (var i = 2; i < nodeArgs.length; i++) {
 
-var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=7fc0d809"; 
+        if (i > 2 && i < nodeArgs.length) {
 
-request(queryURL, function (error, response, body) {
+            movie = movie + "+" + nodeArgs[i];
 
-    // If I have a successful request, it will return a status code of 200 and then move forward with the function 
-    if (!error && response.statusCode === 200) {
+        } else {
 
-        // Parse and recover the rating from imdb
-        console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+            movie += nodeArgs[i];
+
+        }
     }
-});
+
+    var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=7fc0d809"; 
+
+    console.log(queryURL);
+    
+    request(queryURL, function (error, response, body) {
+
+        // If I have a successful request, it will return a status code of 200 and then move forward with the function 
+        if (!error && response.statusCode === 200) {
+
+            // Parse and recover the rating from imdb
+            console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+        }
+    });
+
+// create a varable twitArg and pass the console input argument
+var twitArg = process.argv[2];
+
+if (twitArg === 'my-tweets') {
+    
+    
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+    });
+       
+} else (error) {
+    console.log("This is an error", error);
+}
