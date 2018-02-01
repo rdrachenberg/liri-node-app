fd8d5ab3619c9c4b5a3821d5fb9c02c2fe0b8da9
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-01-31 20:26:09 -0500

    all returns working

--- liri.js
+++ liri.js
@@ -14,7 +14,7 @@ var keys = require('./keys.js');
 var dotenv = require('dotenv');
 
 //dont know if i need the below variable with require 
-// var enviro = require('./.env')
+// var enviro = require('./.env')   
 
 // requrie spotify package and creates a spotify variable 
 var Spotify = require('node-spotify-api');
@@ -22,12 +22,15 @@ var Spotify = require('node-spotify-api');
 // create variable twitter that requires twitter npm package 
 var Twitter = require('twitter');
 
+var fs = require("fs");
+
 // create variable to access key information 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 // create variable with argv, variable for query reqeust from omdb
-// then the actual request 
+// then the actual request f
+
 var nodeArgs = process.argv;
 
 var movie = "";
@@ -43,19 +46,31 @@ var movie = "";
             movie += nodeArgs[i];
 
         }
-    }
+    };
 
     var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=7fc0d809"; 
-
-    console.log(queryURL);
     
     request(queryURL, function (error, response, body) {
 
         // If I have a successful request, it will return a status code of 200 and then move forward with the function 
         if (!error && response.statusCode === 200) {
-
+            var rotten = "Rotten Tomatos"
+            // Parse and recover the Title of the Movie
+            console.log("Title: " + JSON.parse(body).Title);
+            // Parsae and recover the Year the movie was made 
+            console.log("Release Year: " + JSON.parse(body).Year);
             // Parse and recover the rating from imdb
-            console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            // Parse and recover Rotton Tomatos value
+            console.log("Rotten Tomatos: " + JSON.parse(body).Value);
+            // Parse and recover Country where flim was produced 
+            console.log("Country of Production Origin: " + JSON.parse(body).Country);
+            // Parse and recover the Language of the file
+            console.log("Language: " + JSON.parse(body).Language);
+            // Parse and recover the Plot of the movie 
+            console.log("Plot: " + JSON.parse(body).Plot);
+            // Parse and recover the Actors
+            console.log("Actors: " + JSON.parse(body).Actors);
         }
     });
 
@@ -77,6 +92,7 @@ if (twitArg === 'my-tweets') {
 }
 
 var spotArg = process.argv[2];
+
 var spotArgSong = process.argv[3];
 
     if (spotArg === 'spotify-this-song') {
@@ -93,11 +109,19 @@ var spotArgSong = process.argv[3];
             console.log("Preview Link: " + artist.artists[0].external_urls.spotify);
             console.log("Album: " + artist.name);
             
-            // console.log(yourval);
-            //     var songDetails = data.tracks.items[0];
-            // console.log(songDetails.artists[0].name);
-
-            // could also do this for the console:
-            //     console.log("Artist: " + songDetails.artists[0].name);
         });
     };
+var random = process.argv[2];
+
+if (random === 'do-what-it-says') {
+    // This block of code will read from the "random.txt" file.
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+        // We will then print the contents of data
+        console.log(data);
+    });
+};
